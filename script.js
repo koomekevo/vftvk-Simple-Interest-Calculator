ac9f267e2c091f8eb8d86eb6c59fd74378f6913e
Kevin Koome <koome.kevo@gurucrafts.net>
2021-07-24 07:54:29 +0300

    Simple Interest Calculator Project by Kevin Koome

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+//function to calculate interest
 function compute()
-{
-    p = document.getElementById("principal").value;
+{  
+	var principal = document.getElementById("principal").value;
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+	var interest = principal * years * rate / 100;
     
+    //retrieve current date and time
+    var dateNow = new Date();
+    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
+    
+    //display results after calculating interest
+    var resultDisplay = document.getElementById("result");
+    resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
 }
-        
+//function to change value of slider
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+
+
+	
